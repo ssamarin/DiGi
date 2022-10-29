971a31fc604e1292fee52a225e489c60cacd0c24
ssamarin <semenssamarin@mail.ru>
2022-10-29 19:48:41 +1000

    add promo and responsive for promo section

--- src/js/script.js
+++ src/js/script.js
@@ -2,25 +2,27 @@ $('.top-slider').slick({
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
+  nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="icons/arrows/next.svg" alt="next"></button>',
+  prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="icons/arrows/prev.svg" alt="prev"></button>',
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-
+        dots: true,
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 640,
       settings: {
-
+        dots: true,
+        arrows: false
       }
     },
     {
       breakpoint: 480,
       settings: {
-
+        dots: true,
       }
     }
   ]
 });
-		
